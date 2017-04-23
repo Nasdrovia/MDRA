97a9ddf57ffa6d0bb7de8a1ee61a28370cf3de9e
Alex <darecosa@hotmail.com>
2017-04-24 00:27:29 +0200

    Modalites de paiement & Devis dialogs

--- MDRAFX/src/ViewController/ProjetViewerController.java
+++ MDRAFX/src/ViewController/ProjetViewerController.java
@@ -7,6 +7,8 @@ import Model.Projet;
 import Model.User;
 import Utils.JavaFXUtils;
 import application.Session;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -18,7 +20,10 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -40,6 +45,12 @@ public class ProjetViewerController {
 
 	@FXML
 	private ComboBox<User> comboBox;
+
+	@FXML
+	private TableView tableDevis;
+
+	@FXML
+	private TableView tableModalites;
 
 	@FXML
 	private TextArea textDescription;
@@ -147,6 +158,55 @@ public class ProjetViewerController {
 			}
 		});
 		listViewPlans.setItems(FXCollections.observableArrayList(planDAO.findProjetPlans(projet)));
+		fillModalitesPaiement();
+	}
+
+	private void fillDevis() {
+
+	}
+
+	public static class ModalitePaiement {
+		private final SimpleStringProperty palier;
+		private final SimpleFloatProperty taux;
+		private final SimpleFloatProperty cout;
+
+		private ModalitePaiement(String palier, float taux, float cout) {
+			this.palier = new SimpleStringProperty(palier);
+			this.taux = new SimpleFloatProperty(taux);
+			this.cout = new SimpleFloatProperty(cout);
+		}
+
+		public String getPalier() {
+			return palier.get();
+		}
+
+		public float getTaux() {
+			return taux.get();
+		}
+
+		public float getCout() {
+			return cout.get();
+		}
+
+	}
+
+	private void fillModalitesPaiement() {
+		TableColumn columnModalites = (TableColumn) tableModalites.getColumns().get(0);
+		columnModalites.setCellValueFactory(new PropertyValueFactory<ModalitePaiement, String>("palier"));
+		TableColumn columnTaux = (TableColumn) tableModalites.getColumns().get(1);
+		columnTaux.setCellValueFactory(new PropertyValueFactory<ModalitePaiement, String>("taux"));
+		TableColumn columnCout = (TableColumn) tableModalites.getColumns().get(2);
+		columnCout.setCellValueFactory(new PropertyValueFactory<ModalitePaiement, String>("cout"));
+
+		tableModalites.setItems(FXCollections.observableArrayList(new ModalitePaiement("Signature", 3.f, 00.00f),
+				new ModalitePaiement("Obtention du permis de construire", 10.f, 00.00f),
+				new ModalitePaiement("Ouverture du chantier", 15.f, 00.00f),
+				new ModalitePaiement("Achèvement des fondations", 25.f, 00.00f),
+				new ModalitePaiement("Achèvement des murs", 40.f, 00.00f),
+				new ModalitePaiement("Mise hors d'eaux / air", 75.f, 00.00f),
+				new ModalitePaiement("Achèvement des travaux d'équipement", 95.f, 00.00f),
+				new ModalitePaiement("Remise des clés", 100.f, 00.00f)));
+
 	}
 
 	@FXML
